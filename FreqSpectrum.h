de5ff9ab87d57a8ca5a789e2ea9c68ba41ac793c
Shaunak Ranade <136633626+sranade98@users.noreply.github.com>
2025-07-23 17:11:16 -0400

    Update FreqSpectrum.h

--- FreqSpectrum.h
+++ FreqSpectrum.h
@@ -22,12 +22,18 @@ public:
     void setFundamentalFrequency(float freq);
     FreqSpectrum();
     
+
+    
     ~FreqSpectrum() override;
     
     void paint (juce::Graphics&) override;
     void resized() override;
     void drawFrame (juce::Graphics& g);
     void updateSpectrumData (const float* newData, size_t size);
+
+    
+    void setIdealFrequency (float freq, float min, float max);
+    
     
 
 
@@ -35,5 +41,22 @@ private:
     static constexpr size_t scopeSize = 512;  // Same value as MainComponent
     float scopeData[scopeSize] = { 0.0f };
     float fundamentalFreq = -1.0f;
+    
+    float idealFreq = -1.0f;
+    float displayMinFreq = 50.0f;
+    float displayMaxFreq = 500.0f;
+    juce::SmoothedValue<float> smoothedX;
+    
+    struct TrailPoint
+    {
+        float x;
+        float alpha;
+    };
+
+    std::deque<TrailPoint> trail;
+
+    
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FreqSpectrum)
 };
